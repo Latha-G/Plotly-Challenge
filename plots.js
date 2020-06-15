993d8fe09bcd77b28a30d2f1f58248f4b3a73d75
Latha-G <60633643+Latha-G@users.noreply.github.com>
2020-06-15 16:02:49 -0700

    make it better

--- plots.js
+++ plots.js
@@ -8,6 +8,7 @@ function init() {
 
   // Use the list of sample names to populate the select options
   d3.json("samples.json").then((data) => {
+
     var sampleNames = data.names;
 
     sampleNames.forEach((sample) => {
@@ -21,6 +22,7 @@ function init() {
     var firstSample = sampleNames[0];
     buildCharts(firstSample);
     buildMetadata(firstSample);
+
   });
 
 }
@@ -28,10 +30,13 @@ function init() {
 function buildMetadata(sample) {
 
   d3.json("samples.json").then((data) => {
+
     var metadata = data.metadata;
+
     // Filter the data for the object with the desired sample number
     var resultArray = metadata.filter(sampleObj => sampleObj.id == sample);
     var result = resultArray[0];
+
     // Use d3 to select the panel with id of `#sample-metadata`
     var PANEL = d3.select("#sample-metadata");
 
@@ -39,13 +44,11 @@ function buildMetadata(sample) {
     PANEL.html("");
 
     // Use `Object.entries` to add each key and value pair to the panel
-    // Hint: Inside the loop, you will need to use d3 to append new
-    // tags for each key-value in the metadata.
     Object.entries(result).forEach(([key, value]) => {
       PANEL.append("h6").text(`${key.toUpperCase()}: ${value}`);
     });
 
-    // BONUS: Build the Gauge Chart
+    // Build the Gauge Chart
     buildGauge(result.wfreq);
 
   });
@@ -54,7 +57,10 @@ function buildMetadata(sample) {
 function buildCharts(sample) {
 
   d3.json("samples.json").then((data) => {
+
     var samples = data.samples;
+
+    // Filter the data for the object with the desired sample number
     var resultArray = samples.filter(sampleObj => sampleObj.id == sample);
     var result = resultArray[0];
 
@@ -64,12 +70,13 @@ function buildCharts(sample) {
 
     // Build a Bubble Chart
     var bubbleLayout = {
-      title: "Bacteria Cultures Per Sample",
-      margin: { t: 0 },
+      title: "<b>Bacteria Cultures Per Sample</b><br>",
+      margin: { t: 30},
       hovermode: "closest",
       xaxis: { title: "OTU ID" },
       margin: { t: 30}
     };
+
     var bubbleData = [
       {
         x: otu_ids,
@@ -87,6 +94,7 @@ function buildCharts(sample) {
     Plotly.newPlot("bubble", bubbleData, bubbleLayout);
 
     var yticks = otu_ids.slice(0, 10).map(otuID => `OTU ${otuID}`).reverse();
+
     var barData = [
       {
         y: yticks,
@@ -98,18 +106,110 @@ function buildCharts(sample) {
     ];
 
     var barLayout = {
-      title: "Top 10 Bacteria Cultures Found",
+      title: "<b>Top 10 Bacteria Cultures Found</b>",
       margin: { t: 100, l: 150 }
     };
 
     Plotly.newPlot("bar", barData, barLayout);
+
   });
 }
 
+function buildGauge(wfreq) {
+
+  // Enter the washing frequency between 0 and 180
+  var level = parseFloat(wfreq) * 20;
+
+  // Trig to calc meter point
+  var degrees = 180 - level;
+  var radius = 0.5;
+  var radians = (degrees * Math.PI) / 180;
+  var x = radius * Math.cos(radians);
+  var y = radius * Math.sin(radians);
+
+  // Path: may have to change to create a better triangle
+  var mainPath = "M -.0 -0.05 L .0 0.05 L ";
+  var pathX = String(x);
+  var space = " ";
+  var pathY = String(y);
+  var pathEnd = " Z";
+  var path = mainPath.concat(pathX, space, pathY, pathEnd);
+
+  var data = [
+    {
+      type: "scatter",
+      x: [0],
+      y: [0],
+      marker: { size: 12, color: "850000" },
+      showlegend: false,
+      name: "Freq",
+      text: level,
+      hoverinfo: "text+name"
+    },
+    {
+      values: [50 / 9, 50 / 9, 50 / 9, 50 / 9, 50 / 9, 50 / 9, 50 / 9, 50 / 9, 50 / 9, 50],
+      rotation: 90,
+      text: ["8-9", "7-8", "6-7", "5-6", "4-5", "3-4", "2-3", "1-2", "0-1", ""],
+      textinfo: "text",
+      textposition: "inside",
+      marker: {
+        colors: [
+          "rgba(0, 105, 11, .5)",
+          "rgba(10, 120, 22, .5)",
+          "rgba(14, 127, 0, .5)",
+          "rgba(110, 154, 22, .5)",
+          "rgba(170, 202, 42, .5)",
+          "rgba(202, 209, 95, .5)",
+          "rgba(210, 206, 145, .5)",
+          "rgba(232, 226, 202, .5)",
+          "rgba(240, 230, 215, .5)",
+          "rgba(255, 255, 255, 0)"
+        ]
+      },
+      labels: ["8-9", "7-8", "6-7", "5-6", "4-5", "3-4", "2-3", "1-2", "0-1", ""],
+      hoverinfo: "label",
+      hole: 0.5,
+      type: "pie",
+      showlegend: false
+    }
+  ];
+
+  var layout = {
+    shapes: [
+      {
+        type: "path",
+        path: path,
+        fillcolor: "850000",
+        line: {
+          color: "850000"
+        }
+      }
+    ],
+    title: "<b>Belly Button Washing Frequency</b> <br> Scrubs per Week",
+    height: 500,
+    width: 500,
+    xaxis: {
+      zeroline: false,
+      showticklabels: false,
+      showgrid: false,
+      range: [-1, 1]
+    },
+    yaxis: {
+      zeroline: false,
+      showticklabels: false,
+      showgrid: false,
+      range: [-1, 1]
+    }
+  };
+
+  var GAUGE = document.getElementById("gauge");
+  Plotly.newPlot(GAUGE, data, layout);
+}
+
 function optionChanged(newSample) {
+
   // Fetch new data each time a new sample is selected
   buildCharts(newSample);
   buildMetadata(newSample);
+
 }
-
-
